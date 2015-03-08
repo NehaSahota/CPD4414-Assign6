5c50f35865516c9c99aedb65b0da51cf8b0c86dd
Neha <neha.sahota@gmail.com>
2015-03-07 23:27:40 -0500

    change path commit

--- src/java/org/netbeans/rest/application/config/ApplicationConfig.java
+++ src/java/org/netbeans/rest/application/config/ApplicationConfig.java
@@ -13,7 +13,7 @@ import javax.ws.rs.core.Application;
  *
  * @author c0646567
  */
-@javax.ws.rs.ApplicationPath("webresources")
+@javax.ws.rs.ApplicationPath("")
 public class ApplicationConfig extends Application {
 
     @Override
